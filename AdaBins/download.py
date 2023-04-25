3bc1d1d6f4825cd0e7dda4964160869666f1fb65
Ali-Abbas <Ali.abbas.ssj1999@gmail.com>
2023-04-25 12:49:46 +0300

    few changes (just to make sure I can commit).

--- AdaBins/download.py
+++ AdaBins/download.py
@@ -1,10 +1,13 @@
 from infer import InferenceHelper
 from PIL import Image
 import matplotlib.pyplot as plt
+import time
+
+
 infer_helper = InferenceHelper(dataset='nyu')
 
 # predict depth of a single pillow image
-img = Image.open("test_imgs/my room.jpg")  # any rgb pillow image
+img = Image.open("test_imgs/my room 002.jpg")  # any rgb pillow image
 img = img.resize(size=(640, 480))
 bin_centers, predicted_depth = infer_helper.predict_pil(img)
 
